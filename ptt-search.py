d1b003414eb00281dab1fe43b74aaa33122ccad0
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-09-01 15:07:06 -0700

    Add try-except block

--- ptt-search.py
+++ ptt-search.py
@@ -35,37 +35,42 @@ page = 1
 post_list = []
 
 while page <= int(options.pages):
-    # request and parse current page
-    html_doc = requests.get(cur_url)
-    soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-    # post payload if there is over18 check
-    if soup.find('div', {'class': 'over18-notice'}):
-        payload = {
-            'from': cur_url,
-            'yes': 'yes'
-        }
-        reqs = requests.session()
-        html_doc = reqs.post(PTT_OVER_18_URL, data=payload)
-        html_doc = reqs.get(cur_url)
+    try:
+        # request and parse current page
+        html_doc = requests.get(cur_url)
         soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-    prev_url = soup.find('div', {'class': 'btn-group-paging'}).findChildren()[1]['href']
+        # post payload if there is over18 check
+        if soup.find('div', {'class': 'over18-notice'}):
+            payload = {
+                'from': cur_url,
+                'yes': 'yes'
+            }
+            reqs = requests.session()
+            html_doc = reqs.post(PTT_OVER_18_URL, data=payload)
+            html_doc = reqs.get(cur_url)
+            soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-    # fetch posts from current page
-    for post in soup.find_all('div', {'class': 'r-ent'}):
-        post_title = post.find('div', {'class': 'title'})
-        title = post_title.text
-        link = 'URL not found' if not post_title.find('a') else PTT_BASE_URL + post_title.find('a')['href']
-        if options.category in title and options.keyword in title:
-            date = post.find('div', {'class': 'date'}).text
-            author = post.find('div', {'class': 'author'}).text
-            post_list.append({'title': title.strip(),
-                              'link': link.strip(),
-                              'date': date.strip(),
-                              'author': author.strip()})
-    cur_url = PTT_BASE_URL + prev_url
-    page += 1
+        prev_url = soup.find('div', {'class': 'btn-group-paging'}).findChildren()[1]['href']
+
+        # fetch posts from current page
+        for post in soup.find_all('div', {'class': 'r-ent'}):
+            post_title = post.find('div', {'class': 'title'})
+            title = post_title.text
+            link = 'URL not found' if not post_title.find('a') else PTT_BASE_URL + post_title.find('a')['href']
+            if options.category in title and options.keyword in title:
+                date = post.find('div', {'class': 'date'}).text
+                author = post.find('div', {'class': 'author'}).text
+                post_list.append({'title': title.strip(),
+                                  'link': link.strip(),
+                                  'date': date.strip(),
+                                  'author': author.strip()})
+        cur_url = PTT_BASE_URL + prev_url
+        page += 1
+    except:
+        print('Ok, something went wrong :(')
+        exit()
 
 # print fetched posts and links
 for post in post_list:
