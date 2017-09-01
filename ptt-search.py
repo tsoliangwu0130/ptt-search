e87c17297760bc6a81ef122d47d21dce9b39a497
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-09-01 13:55:13 -0700

    Add page filter

--- ptt-search.py
+++ ptt-search.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from optparse import OptionParser
 
+# constant variables
 PTT_BASE_URL = 'https://www.ptt.cc'
 
 # option parser
@@ -20,18 +21,34 @@ parser.add_option("-k",
                   help="post keyword",
                   default='',
                   metavar="<keyword>")
+parser.add_option("-p",
+                  dest="pages",
+                  help="number of pages",
+                  default=1,
+                  metavar="<pages>")
 (options, args) = parser.parse_args()
 
-html_doc = requests.get('{}/bbs/{}'.format(PTT_BASE_URL, options.board))
-soup = BeautifulSoup(html_doc.text, 'html.parser')
-
+# intial variables
+cur_url = '{}/bbs/{}'.format(PTT_BASE_URL, options.board)
+page = 1
 post_list = []
-for post in soup.find_all('div', {'class': 'title'}):
-    title = post.text
-    link = post.find('a')['href']
-    if options.category in title and options.keyword in title:
-        post_list.append({'title': title, 'link': PTT_BASE_URL + link})
 
-for post in post_list:
+while page <= int(options.pages):
+    # request and parse current page
+    html_doc = requests.get(cur_url)
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+    prev_url = soup.find('div', {'class': 'btn-group-paging'}).findChildren()[1]['href']
+
+    # fetch posts from current page
+    for post in soup.find_all('div', {'class': 'title'}):
+        title = post.text
+        link = 'URL not found.' if not post.find('a') else post.find('a')['href']
+        if options.category in title and options.keyword in title:
+            post_list.append({'title': title, 'link': PTT_BASE_URL + link})
+    cur_url = PTT_BASE_URL + prev_url
+    page += 1
+
+# print fetched posts and links
+for idx, post in enumerate(post_list):
     print(post['title'])
     print(post['link'])
