3a6f79c8d42e3bc16f0a02a567d685b160726de8
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-09-01 11:29:42 -0700

    Add query option

--- ptt-search.py
+++ ptt-search.py
@@ -7,10 +7,13 @@ PTT_BASE_URL = 'https://www.ptt.cc'
 # option parser
 parser = OptionParser()
 parser.add_option("-b",
-                  "--board",
                   dest="board",
-                  help="Board name to search",
+                  help="board name to search",
                   metavar="<board name>")
+parser.add_option("-q",
+                  dest="query",
+                  help="query for post title",
+                  metavar="<query>")
 (options, args) = parser.parse_args()
 
 html_doc = requests.get('{}/bbs/{}'.format(PTT_BASE_URL, options.board))
@@ -20,7 +23,8 @@ post_list = []
 for post in soup.find_all('div', {'class': 'title'}):
     title = post.text
     link = post.find('a')['href']
-    post_list.append({'title': title, 'link': PTT_BASE_URL + link})
+    if options.query in title:
+        post_list.append({'title': title, 'link': PTT_BASE_URL + link})
 
 for post in post_list:
     print(post['title'])
